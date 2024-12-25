290ba108a054eb1243fdd78626e0a9bb62a24a59
Neetigupta274 <guptaneeti042@gmail.com>
2024-12-25 13:51:07 +0530

    Modified backend config/database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize("neweauction", "root", "khan@123", {
+const sequelize = new Sequelize("sqldatabase", "root", "root", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
