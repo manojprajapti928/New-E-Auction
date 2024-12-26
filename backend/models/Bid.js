a51181848216ecf5827a0cf49808bd7cf54d58fc
manojprajapti928 <manojprajapti928@gmail.com>
2024-12-26 14:14:37 +0530

    mmmmm

--- backend/models/Bid.js
+++ backend/models/Bid.js
@@ -1,7 +1,40 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const User = require('./User');
+const Auction = require('./Auction');
+const Product = require('./Product');
 
 const Bid = sequelize.define('Bid', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    // references: {
+    //   model: User,
+    //   key: 'id',
+    // },
+  },
+  auctionId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    // references: {
+    //   model: Auction,
+    //   key: 'id',
+    // },
+  },
+  productId:{
+    type: DataTypes.INTEGER,
+    references: {
+      model: Product,
+      key: 'id',
+    },
+   
+  },
   amount: {
     type: DataTypes.FLOAT,
     allowNull: false,
@@ -10,6 +43,11 @@ const Bid = sequelize.define('Bid', {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW,
   },
+
 });
+
+Product.hasMany(Bid, { foreignKey: 'productId' });
+Bid.belongsTo(Product, { foreignKey: 'productId' });
+
 
 module.exports = Bid;
