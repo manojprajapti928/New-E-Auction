bfc1f58f42a875349b2f658c90823d5d85ba057f
Aaisha Khan <160912968+Aaisha011@users.noreply.github.com>
2024-12-31 11:42:29 +0530

    updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const { register, login, getAllUsers, deleteUser } = require('../controllers/userController');
-const { isAuthenticated, isAdmin } = require('../middleware/auth');
-const { upload } = require('../utils/multer');
+const express = require("express");
+const {
+  register,
+  login,
+  getAllUsers,
+  deleteUser,
+  getUserDetails,
+} = require("../controllers/userController");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
+const { upload } = require("../utils/multer");
 // const multer = require('multer');
 // const upload = multer();
 
@@ -9,9 +15,10 @@ const router = express.Router();
 
 // all api done work properly
 
-router.post('/register', upload, register);
-router.post('/login',login);
-router.get('/users', isAuthenticated, isAdmin, getAllUsers);
-router.delete('/users/:id', isAuthenticated, isAdmin, deleteUser);
+router.post("/register", upload, register);
+router.post("/login", login);
+router.get("/users", isAuthenticated, isAdmin, getAllUsers);
+router.delete("/users/:id", isAuthenticated, isAdmin, deleteUser);
+router.get("/getUserDetails", isAuthenticated, getUserDetails);
 
 module.exports = router;
